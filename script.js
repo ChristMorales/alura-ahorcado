0ab16075d278c2b7b699db17a75259d6b9c134ea
ChristMorales <88733836+ChristMorales@users.noreply.github.com>
2022-05-24 23:24:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ btnIniciar.addEventListener("click", function(){
 btnAgregar.addEventListener("click", function(){
 	console.log("boton agregar");
 });
+
+let palabras = ["python", "monitor", "cafetera", "termo", "salame", "jamon", "remoto", "pizarron", "frontend", "backend"];
+let intentos = [];
+let exito = []
+let fallo = []
+let palabraActual = []
+
+function seleccionarRandom(){
+	let palabra = palabras[Math.floor(Math.random() * palabras.length)];
+	console.log(palabra);
+	palabraActual = Array.from(palabra);
+}
+
+function checarLetra(letra){
+	let indeceLetra = 0;
+	if (palabraActual.includes(letra)){
+		console.log("exito");
+
+		intentos.push(letra);
+		exito.push(letra);
+		indiceLetra = palabraActual.indexOf(letra);
+	} else {
+		console.log("fallo");
+		intentos.push(letra);
+		fallo.push(letra);
+	}
+	return indiceLetra;
+}
